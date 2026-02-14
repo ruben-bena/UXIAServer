ef4126a8c79e2e81c781fd8dc850a7770c786955
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2026-02-14 15:54:40 +0100

    Hardcoded userId from test user in Request consolidation

--- src/controllers/analyzeImageController.js
+++ src/controllers/analyzeImageController.js
@@ -1,4 +1,5 @@
 const { logger } = require('../config/logger');
+const { Request } = require('../models');
 
 const analyzeImage = async (req, res) => {
   try {
@@ -11,13 +12,14 @@ const analyzeImage = async (req, res) => {
 
     // Validación del body
     if (!prompt?.trim()) {
-            logger.warn('El campo "prompt" está vacío --> retornando error a cliente');
-            return res.status(400).json({ message: 'Campo "prompt" es obligatorio' });
+      logger.warn('El campo "prompt" está vacío --> retornando error a cliente');
+      return res.status(400).json({ message: 'Campo "prompt" es obligatorio' });
     };
 
     // Consolidar objeto Request
     logger.debug('[analyzeImageController] Consolidando objeto Request...');
     const newRequest = await Request.create({
+      userId: "4789e020-09b4-11f1-84ff-f6f0c5648c52", // userId for 'testUser'
       prompt: prompt
     });
     logger.debug('[analyzeImageController] Objeto Request consolidado con éxito.');
