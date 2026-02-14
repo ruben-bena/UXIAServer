7a1dfe1734eb545982c8e1211e0353d28bdd86db
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2026-02-14 12:32:24 +0100

    Implemented consolidation of Request in call /api/analitzar-imatge

--- src/controllers/analyzeImageController.js
+++ src/controllers/analyzeImageController.js
@@ -2,6 +2,32 @@ const { logger } = require('../config/logger');
 
 const analyzeImage = async (req, res) => {
   try {
+    const {
+      imageBase64,
+      prompt,
+      model,
+      stream
+    } = req.body;
+
+    // Validación del body
+    if (!prompt?.trim()) {
+            logger.warn('El campo "prompt" está vacío --> retornando error a cliente');
+            return res.status(400).json({ message: 'Campo "prompt" es obligatorio' });
+    };
+
+    // Consolidar objeto Request
+    logger.debug('[analyzeImageController] Consolidando objeto Request...');
+    const newRequest = await Request.create({
+      prompt: prompt
+    });
+    logger.debug('[analyzeImageController] Objeto Request consolidado con éxito.');
+
+    // Petición a la IA
+
+    // Consolidar respuesta de la IA en objeto Response
+
+    // Retornar respuesta
+
     logger.debug('Entro en función "analyzeImage"');
     return res.json({
       status: 'OK',
