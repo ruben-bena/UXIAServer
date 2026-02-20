f522d85f283d042487ddf9a5ae6d0b8bd544e9e4
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2026-02-20 19:19:14 +0100

    Implemented finding user with same phoneNumber in call Validar

--- src/controllers/validateUserController.js
+++ src/controllers/validateUserController.js
@@ -12,8 +12,21 @@ const validateUser = async (req, res) => {
         } = req.body;
 
         // Compare body validationCode with stored one
+        // Buscar usuario con mismo número de teléfono
+        logger.debug(`[registerUserController] Buscando usuario con mismo telefono (phoneNumber=${phoneNumber})...`);
+        const userWithPhoneNumber = User.findOne({
+            where: {
+                phoneNumber: phoneNumber
+            }
+        });
+        // TODO: Contemplar caso de no encontrar usuario con ese número
+        logger.debug(`[registerUserController] Usuario encontrado (username=${userWithPhoneNumber.username}`);
 
-        // If is valid, return Token
+        // Comparar código recibido con código guardado
+        
+
+        // Si el código es válido, retornamos token
+        // En caso contrario, retornamos error
 
         // Retornar respuesta
         return res.status(200).json({
