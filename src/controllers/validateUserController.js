2808e00a6e6d69033e8d53918743c0c622ba7f7d
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2026-02-20 20:06:28 +0100

    Implemented all of /api/usuaris/validar

--- src/controllers/validateUserController.js
+++ src/controllers/validateUserController.js
@@ -1,5 +1,7 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET || 'clau_dev';
 const { logger } = require('../config/logger');
-const { User } = require('../models');
+const { User, Token } = require('../models');
 const fs = require('fs').promises;
 require('dotenv').config();
 
@@ -11,31 +13,74 @@ const validateUser = async (req, res) => {
             validationCode
         } = req.body;
 
-        // Compare body validationCode with stored one
         // Buscar usuario con mismo número de teléfono
-        logger.debug(`[registerUserController] Buscando usuario con mismo telefono (phoneNumber=${phoneNumber})...`);
+        logger.debug(`[registerUserController] Buscando usuario con mismo telefono (phoneNumber="${phoneNumber}")...`);
         const userWithPhoneNumber = await User.findOne({
             where: {
-                phoneNumber: phoneNumber
+                phoneNumber: phoneNumber,
+                isValidated: false
             }
         });
-        // TODO: Contemplar caso de no encontrar usuario con ese número
-        logger.debug(`[registerUserController] Usuario encontrado (username=${userWithPhoneNumber.username})`);
+        // Contemplar caso de no encontrar usuario con ese número
+        if (!userWithPhoneNumber) {
+            logger.debug(`[registerUserController] Usuario con phoneNumber="${phoneNumber}" no encontrado. Retornando error...`);
+            return res.status(400).json({
+                status: 'ERROR',
+                message: 'No user found with that phoneNumber'
+            });
+        }
+        logger.debug(`[registerUserController] Usuario encontrado (username="${userWithPhoneNumber.username}")`);
 
         // Comparar código recibido con código guardado
-
-
+        // 
         // Si el código es válido, retornamos token
         // En caso contrario, retornamos error
+        const isValidCode = (validationCode == userWithPhoneNumber.validationCode);
+        logger.debug(`[registerUserController] Usuario válido --> ${isValidCode}`);
+        if (isValidCode) {
+            // Validar usuario
+            logger.debug(`[registerUserController] Validando usuario...`);
+            userWithPhoneNumber.validationCode = null;
+            userWithPhoneNumber.isValidated = true;
+            await userWithPhoneNumber.save();
+            logger.debug(`[registerUserController] Usuario persistido como valido.`);
 
-        // Retornar respuesta
-        return res.status(200).json({
-            status: 'OK',
-            message: 'User correctly validated',
-            data: {
-                'token': 'pepe'
-            }
-        });
+            // Crear token y guardarlo en su tabla
+            logger.debug(`[registerUserController] Generando Token...`);
+            await Token.destroy({
+                where: { userId: userWithPhoneNumber.userId }
+            });
+            logger.debug(`[registerUserController] Tokens asociados al usuario borrados.`);
+            const tokenValue = jwt.sign(
+                {
+                    userId: userWithPhoneNumber.userId,
+                    phoneNumber: userWithPhoneNumber.phoneNumber,
+                    isAdministrator: userWithPhoneNumber.isAdministrator || false
+                },
+                JWT_SECRET
+            );
+            await Token.create({
+                userId: userWithPhoneNumber.userId,
+                token: tokenValue
+            });
+            logger.debug(`[registerUserController] Nuevo Token generado y persistido.`);
+
+            // Retornar token a cliente
+            logger.debug(`[registerUserController] Retornando nuevo Token al usuario...`);
+            return res.status(200).json({
+                status: 'OK',
+                message: 'User correctly validated',
+                data: {
+                    'api_key': tokenValue
+                }
+            });
+        } else {
+            logger.debug(`[registerUserController] Retornando error al usuario debido a código invalido...`);
+            return res.status(400).json({
+                status: 'ERROR',
+                message: 'validationCode received does not match with persisted one'
+            });
+        }
     } catch (error) {
         logger.error(error);
         return res.status(500).json({
